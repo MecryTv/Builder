0887dce60bad52b4243508f7c1ee5e759281ff29
MecryTv <125377912+MecryTv@users.noreply.github.com>
2023-11-10 17:24:36 +0100

    Update ActionbarManager.java

--- ActionbarManager.java
+++ ActionbarManager.java
@@ -4,7 +4,7 @@ import net.md_5.bungee.api.ChatMessageType;
 import net.md_5.bungee.api.chat.TextComponent;
 import org.bukkit.entity.Player;
 
-public class ActionbarManager {
+public class ActionbarBuilder {
 
     public static void sendActionbar(Player p, String message) {
         p.spigot().sendMessage(ChatMessageType.ACTION_BAR, new TextComponent(message));
